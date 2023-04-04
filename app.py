96d194afd1b6c13cef79c95077ebba0093bf5583
EC2 Default User <ec2-user@ip-172-31-89-223.ec2.internal>
2023-04-04 15:33:13 +0000

    docker file

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Welcome to Python Flask Application - Ashok IT"
+    return "Welcome to Python Flask Application - CODINGRAD-DEVOPS-COURSE"
 
 
 if __name__ == "__main__":
